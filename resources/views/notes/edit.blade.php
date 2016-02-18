2183cc6daa4c9b497f7f0ebf61aafb189a06af17
Jim Witte <jim@thunderingbison.com>
2016-02-18 15:09:04 -0600

    revisions to edit.blade.php format. Taller textarea, removed edit title

--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.app') 
 @section('content') 
 <div class="container">
-	<h1>
-		Edit Note
-	</h1>
 	<form method="POST" action="{{$url = route('notes.update', ['id' => $note->id]) }}">
 	{{ csrf_field() }}
 	{{ method_field('PUT') }}
@@ -14,7 +11,7 @@
 		</div>
 		<div class="form-group">
 			<label for="body">Note Body</label>
-			<textarea name="body" id="body" class="form-control">{{$note->body}}</textarea>
+			<textarea name="body" id="body" class="form-control" rows="20">{{$note->body}}</textarea>
 		</div>
 		<div class="form-group">
 			<button type="submit" name="submit" class="btn btn-primary">Update Note</button>
