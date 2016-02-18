0b82649f466fc854561fa32a4eb6825c8c029759
Jim Witte <jim@thunderingbison.com>
2016-02-18 15:12:31 -0600

    removed logged in message on home view. Changed list title to Your Notes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,7 @@
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
 				<div class="panel-heading">
-					Dashboard
-				</div>
-				<div class="panel-body">
-					You are logged in! 
+					Your Notes
 				</div>
 				<div class="list-group">
 					@foreach ($notes as $note)
