6d7e5dddc9c1836f401b0e20edf9629c3984914a
Jim Witte <jim@thunderingbison.com>
2016-02-18 14:48:55 -0600

    revised placement of delete/edit buttons on show.blade.php view

--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -1,35 +1,21 @@
 @extends('layouts.app') @section('content') 
 <div class="container">
-	<div align="right">
-		<form method="POST" class="notes_inline_block form-inline" action="{{$url = route('notes.destroy', ['id' => $note->id]) }}">
-			{{ method_field('delete') }} {{ csrf_field() }} 
-			<button 
-				class="btn btn-danger" 
-				name="delete" 
-				title="delete" 
-				value="delete"
-				onClick="return confirm('Delete this Note?');"
-			>
-				<span class="glyphicon glyphicon-trash" aria-hidden="true">
-				</span>
-			</button>
-		</form>
-		<a class="btn btn-primary" title="edit" href="{{$url = route('notes.edit', ['id' => $note->id]) }}" role="button">
-		<span class="glyphicon glyphicon-edit" aria-hidden="true">
-		</span>
-	</a>
-	</div>
-	<div>
 	<h1 class="notes_no_gap">
 		{{$note->title}} 
 	</h1>
-	<small>
-		<p>{{date_format($note->updated_at, 'Y-m-d H:i:s')}}<br />
-		{{ $note->updated_at->diffForHumans()}}</p>
-	</small>
-</div>
-<div>
-	{{$note->body}} 
-</div>
+	<div style="line-height: .9em;">
+	</form>
+	<a class="btn btn-primary notes_inline_block" title="edit" href="{{$url = route('notes.edit', ['id' => $note->id]) }}" role="button"> <span class="glyphicon glyphicon-edit" aria-hidden="true"> Edit </span> </a> 
+	<form method="POST" class="notes_inline_block form-inline" action="{{$url = route('notes.destroy', ['id' => $note->id]) }}">
+		{{ method_field('delete') }} {{ csrf_field() }} <button class="btn btn-danger" name="delete" title="delete" value="delete" onClick="return confirm('Delete this Note?');"> <span class="glyphicon glyphicon-trash" aria-hidden="true"> Delete </span> </button> <small> 
+		<p class="notes_inline_block">
+			{{date_format($note->updated_at, 'Y-m-d H:i:s')}}<br />
+			{{ $note->updated_at->diffForHumans()}} 
+		</p>
+		</small> 
+	</div>
+	<div style="margin-top: 1.5em;">
+		{{$note->body}} 
+	</div>
 </div>
 @endsection 
