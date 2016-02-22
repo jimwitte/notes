35e49b243f9bf7a4d8bcd8564a668c785517eec2
Jim Witte <jim@thunderingbison.com>
2016-02-22 11:29:02 -0600

    added introductory text to app welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,13 @@
                 <div class="panel-heading">Welcome to Notes</div>
 
                 <div class="panel-body">
-                    Notes is a web application 
+                    <strong>Notes</strong> is a web application where you can write and edit notes using Markdown. App features include:
+                    <ul>
+                    	<li>multiple users, with registration and authentication, each user has their own space</li>
+                    	<li>markdown editor with preview</li>
+                    	<li>note tagging</li>
+                    </ul>
+                    <p>To get started, log in or register.</p>
                 </div>
             </div>
         </div>
