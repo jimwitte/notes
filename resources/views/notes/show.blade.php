23f06f76f6943277be4bed0cf379d8b617a65024
Jim Witte <jim@thunderingbison.com>
2016-02-21 21:38:30 -0600

    slight reorganization of edit/delete and last update on show view

--- resources/views/notes/show.blade.php
+++ resources/views/notes/show.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app') @section('content') 
 <div class="container">
 	<div class="col-md-10 col-md-offset-1">
-		<div style="line-height: .9em;">
-			<a class="btn btn-success notes_inline_block" title="edit" href="{{$url = route('notes.edit', ['id' => $note->id]) }}" role="button">
+		<div>
+			<a class="btn btn-success btn-sm notes_inline_block" title="edit" href="{{$url = route('notes.edit', ['id' => $note->id]) }}" role="button">
 				<span class="glyphicon glyphicon-edit" aria-hidden="true">
 					Edit 
 				</span>
 			</a>
 			<form method="POST" class="notes_inline_block form-inline" action="{{$url = route('notes.destroy', ['id' => $note->id]) }}">
 				{{ method_field('delete') }} {{ csrf_field() }} 
-				<button class="btn btn-danger" name="delete" title="delete" value="delete" onClick="return confirm('Delete this Note?');">
+				<button class="btn btn-danger btn-sm" name="delete" title="delete" value="delete" onClick="return confirm('Delete this Note?');">
 					<span class="glyphicon glyphicon-trash" aria-hidden="true">
 						Delete 
 					</span>
 				</button>
 			</form>
 			<small>
-				<p class="notes_inline_block">
-					{{date_format($note->updated_at, 'Y-m-d H:i:s')}}<br /> {{ $note->updated_at->diffForHumans()}} 
+				<p>
+					Updated: {{date_format($note->updated_at, 'Y-m-d H:i:s')}} | {{ $note->updated_at->diffForHumans()}} 
 				</p>
 			</small>
 		</div>
